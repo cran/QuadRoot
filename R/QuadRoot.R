de7528504bcb42446cb88b544f521b406e3b2c5c
Pankaj Das <pankaj.das2@icar.gov.in>
2021-09-10 10:50:06 +0000

    version 0.2.0

--- R/QuadRoot.R
+++ R/QuadRoot.R
@@ -1,11 +1,21 @@
-QuadRoot=function(inputdata)
+QuadRoot <- function(inputdata)
 {
-  a=inputdata[1]
-  b=inputdata[2]
-  c=inputdata[3]
-  t=b^2-4*a*c
-  x1=((-b)+sqrt(t))/(2*a)
-  x2=((-b)-sqrt(t))/(2*a)
-  output=data.frame(x1,x2)
-  return(output)
+  a <- inputdata[1]
+  b <- inputdata[2]
+  c <- inputdata[3]
+  t <- b^2-4*a*c
+  if(t< 0) {
+    return(paste0("This quadratic equation has no real numbered roots."))
+  }
+  else if(t > 0) {
+    x_int_plus <- (-b + sqrt(t)) / (2*a)
+    x_int_neg <- (-b - sqrt(t)) / (2*a)
+    return(paste0("The two x-intercepts for the quadratic equation are ",
+                  format(round(x_int_plus, 4), nsmall = 4), " and ",
+                  format(round(x_int_neg, 4), nsmall = 4), "."))
+  }
+  else #t = 0  case
+    x_int <- (-b) / (2*a)
+  return(paste0("The quadratic equation has only one root. This root is ",
+                x_int))
 }
